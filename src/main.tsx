d3613f2a99d45a90d35ad599c79be28b1434deeb
Jaydeep Chovatiya <jchovatiya316@rku.ac.in>
2026-06-15 23:33:51 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./app/App.tsx";
 import "./styles/index.css";
 
 createRoot(document.getElementById("root")!).render( <BrowserRouter> <App /> </BrowserRouter>
+);createRoot(document.getElementById("root")!).render(
+  <HashRouter>
+    <App />
+  </HashRouter>
 );
